294ec083f3522f9f0683ed9b1b9d0817561ca7a9
Xuan Yan <xuanyan@xuanyan.ws>
2018-07-04 17:32:27 +0800

    16-point fan tests and fix bugs

---
+++ test/16.mjs
@@ -0,0 +1,79 @@
+import assert from 'assert'
+import { getScore } from './utils'
+
+export default {
+  清龙() {
+    let result = getScore(
+      {characters: '1234'},
+      {character: 4},
+      {chows: [{character: 2}, {character: 5}, {character: 8}]}
+    )
+    assert.equal(result.fans.includes('清龙'), true)
+    assert.equal(
+      result.fans.includes('一般高') + result.fans.includes('连六') + result.fans.includes('老少副'),
+      1
+    )
+  },
+  三色双龙会() {
+    let result = getScore(
+      {characters: '13', dots: '55', bamboo: '789'},
+      {character: 2},
+      {chows: [{character: 8}, {bamboo: 2}]}
+    )
+    assert.equal(result.fans.includes('三色双龙会'), true)
+    assert.equal(result.fans.includes('平和'), false)
+    assert.equal(result.fans.includes('喜相逢'), false)
+    assert.equal(result.fans.includes('老少副'), false)
+    assert.equal(result.fans.includes('无字'), false)
+  },
+  一色三步高() {
+    let result1 = getScore(
+      {characters: '2348', dots: '567'},
+      {character: 8},
+      {chows: [{character: 2}, {character: 4}]}
+    )
+    assert.equal(result1.fans.includes('一色三步高'), true)
+    let result2 = getScore(
+      {characters: '5678', dots: '567'},
+      {character: 8},
+      {chows: [{character: 2}, {character: 4}]}
+    )
+    assert.equal(result2.fans.includes('一色三步高'), true)
+  },
+  全带五() {
+    let result = getScore(
+      {characters: '55', dots: '345', bamboo: '57'},
+      {bamboo: 6},
+      {
+        chows: [{dot: 5}],
+        pungs: [{bamboo: 5}]
+      }
+    )
+    assert.equal(result.fans.includes('全带五'), true)
+    assert.equal(result.fans.includes('断幺'), false)
+    assert.equal(result.fans.includes('无字'), false)
+  },
+  三同刻() {
+    let result = getScore(
+      {characters: '44', dots: '444', bamboo: '88'},
+      {character: 4},
+      {
+        chows: [{character: 6}],
+        pungs: [{bamboo: 4}]
+      }
+    )
+    assert.equal(result.fans.includes('三同刻'), true)
+    assert.equal(result.fans.includes('无字'), false)
+  },
+  三暗刻() {
+    let result = getScore(
+      {characters: '33456', dots: '55888'},
+      {character: 3},
+      {
+        concealedKongs: [{bamboo: 9}],
+        isSelfDrawn: true
+      }
+    )
+    assert.equal(result.fans.includes('三暗刻'), true)
+  }
+}
--- test/index.mjs
+++ test/index.mjs
@@ -3,13 +3,15 @@ import testCase_64 from './64'
 import testCase_48 from './48'
 import testCase_32 from './32'
 import testCase_24 from './24'
+import testCase_16 from './16'
 
 let testCaseFan = [
   [88, testCase_88],
   [64, testCase_64],
   [48, testCase_48],
   [32, testCase_32],
-  [24, testCase_24]
+  [24, testCase_24],
+  [16, testCase_16]
 ]
 
 describe('Fan', function() {
